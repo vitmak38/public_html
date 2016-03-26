15409669cd931ff1996865d7bc7c3533256e8ace
vitmak38 <vitmak38@gmail.com>
2016-03-26 14:55:56 +0300

    test

--- setting.php
+++ setting.php
@@ -6,7 +6,7 @@ define('DB', 'public_html');
 
 define('ADMIN_PASS', 'vvxv38ru'); // Пароль от админки
 
-//Эти параметры для работы с Яндекс.Метрикой (23 серия)
+
 define('YANDEX_ID', '');
 define('YANDEX_TOKEN', '');
 ?>
